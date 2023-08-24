402e650952a124147662e6e1b79bbb3c04942f79
JamilErasmo <jamilerasmoq@gmail.com>
2023-08-23 23:02:29 -0500

    final2b

--- proyecto-flask/app.py
+++ proyecto-flask/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template ("master.html")
 
 
 @app.route("/Persona")
